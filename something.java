b2deac32fe1234682c32133dec6520d14ea5dc04
ShadowIsHim <148368990+ShadowIsHim@users.noreply.github.com>
2023-10-18 17:52:24 +0100

    Update something.java

--- something.java
+++ something.java
@@ -1,5 +1,5 @@
 public class something {
     public static void main(String[] args) {
-        System.out.println("reyna is the best");
+        System.out.println("reyna is the best jvghghvghchc");
     }
 }
